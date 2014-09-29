48cdf1638cc56df5f651e492386e087b7fab0675
tacksoo <tacksoo.im@gmail.com>
2014-09-28 23:56:35 -0400

    Update mem.c
    added malloc example

--- mem.c
+++ mem.c
@@ -1,6 +1,30 @@
+#include<stdlib.h>  // dynamic memory allocations are defined here
+#include<stdio.h>
 
-malloc()
+int main()
+{
+    int count;
+    int i;
+    float *prices;
+    printf("Enter the number of items you need summed up: ");
+    scanf("%d",&count);
 
-calloc()
+    prices = (float *) malloc(count * sizeof(float));
 
-memset()
+    if(prices == NULL)
+    {
+        printf("Allocation failure");
+        exit(1);
+    }
+
+    for(i=0; i<count; i++)
+    {
+        printf("Enter price of item\n");
+        scanf("%f",prices+i);  // &prices[i] in array syntax
+    }
+
+    for(i=0; i<count; i++)
+    {
+        printf("%f\n",*(prices+i)); // prices[i] in array syntax
+    }
+}
