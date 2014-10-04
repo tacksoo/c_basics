8f39c6b8402ab59c9d532a596b0533f9b2f04952
dcofer44 <dcofer@ggc.edu>
2014-10-04 13:44:19 -0400

    Adjusted input.c to handle buffer overflows using getline

--- input.c
+++ input.c
@@ -3,9 +3,12 @@
 int main()
 {
     puts("Enter your name");
-    char name[80];
-    fgets(name, sizeof(name), stdin);
+    char* name;
+    size_t size;
+
+    int read = getline(&name, &size, stdin);
     printf("Your name is: %s", name);
+    printf("You input %d chars including \\n\n", read);
 
     return 0;
 }
