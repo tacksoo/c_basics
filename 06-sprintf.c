19fda1354b0050c38e240f5671bbf8171591e38c
Tacksoo Im <tacksoo.im@gmail.com>
2015-05-26 14:45:55 -0400

    Update 06-sprintf.c

--- 06-sprintf.c
+++ 06-sprintf.c
@@ -9,6 +9,8 @@ int main()
 	// itoa(12345,str, 10);
 	// instead use sprintf(), but if you are using MINGW you 
 	// might as well use itoa()
+	
+	// sprintf is print as string 
 	sprintf(str,"%d",1234567);
 	
 	printf("The string is %s \n",str);	
