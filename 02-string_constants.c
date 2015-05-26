df683a7eac8d0b9c943e6195b66d92e7ea9e5d33
Tacksoo Im <tacksoo.im@gmail.com>
2015-05-26 14:20:13 -0400

    Update 02-string_constants.c

--- 02-string_constants.c
+++ 02-string_constants.c
@@ -4,7 +4,11 @@ int main()
 {
 	char c = 'A';
 	char *mystring = "hello world";	
+	// can change 
+	// mystring = "goodbye world";
 	char strs[] = "HELLO WORLD";
+	// cannot change
+	// strs = "GOODBYE WOLRD"; 
 	strs[3]= 'X';
 
 	printf("%s \n", strs);
